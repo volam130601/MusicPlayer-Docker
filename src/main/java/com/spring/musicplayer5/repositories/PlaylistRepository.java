fb5b0a19b521561d002128434b3ba99666bf4061
Admin <volam130601@gmail.com>
2022-07-19 10:44:10 +0700

    Update file Upload

--- src/main/java/com/spring/musicplayer5/repositories/PlaylistRepository.java
+++ src/main/java/com/spring/musicplayer5/repositories/PlaylistRepository.java
@@ -2,6 +2,7 @@ package com.spring.musicplayer5.repositories;
 
 import com.spring.musicplayer5.entity.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.Optional;
 public interface PlaylistRepository extends JpaRepository<Playlist , Long> {
     Optional<Playlist> findByName(String name);
 
-    @Query("select pl from Playlist pl where pl.user.username = ?1")
+    @Query(value = "select * from playlist where username = ?1", nativeQuery = true)
     List<Playlist> findPlaylistByUsername(String username);
 
 }
