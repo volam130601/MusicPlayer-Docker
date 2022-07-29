5ae890e53d60358dd29388611015ccb62d19a255
internenclave123 <volam13062002@gmail.com>
2022-07-29 13:45:13 +0700

    Done task API Artist

--- src/main/java/com/spring/musicplayer5/controllers/impl/ArtistControllerImpl.java
+++ src/main/java/com/spring/musicplayer5/controllers/impl/ArtistControllerImpl.java
@@ -2,7 +2,9 @@ package com.spring.musicplayer5.controllers.impl;
 
 import com.spring.musicplayer5.dto.ResponseObject;
 import com.spring.musicplayer5.entity.Artist;
+import com.spring.musicplayer5.entity.Track;
 import com.spring.musicplayer5.services.ArtistService;
+import com.spring.musicplayer5.services.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,19 +12,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/Artist")
+@RequestMapping("/api/artist")
 public class ArtistControllerImpl {
     @Autowired
     private ArtistService artistService;
 
+    @Autowired
+    private TrackService trackService;
+
     @GetMapping
     public ResponseEntity<ResponseObject> getTop5Artist() {
-        Page<Artist> artists = artistService.findAll(PageRequest.of(0 , 5));
+        Page<Artist> artists = artistService.findAll(PageRequest.of(0, 5));
         return ResponseEntity.status(HttpStatus.OK).body(
-            new ResponseObject("OK" , "Get 5 Artist Hot", artists.toList() , artists.getSize())
+                new ResponseObject("OK", "Get 5 Artist Hot", artists .toList(), artists.toList().size())
+        );
+    }
+
+    @GetMapping("/get_page")
+    public ResponseEntity<ResponseObject> getPageArtist(@RequestParam int page ,
+                                                        @RequestParam int size) {
+        Page<Artist> artists = artistService.findAll(PageRequest.of(page, size));
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("OK", "Get paging success!", artists.toList(), artists.toList().size() , page , size)
+        );
+    }
+
+    @GetMapping("/get_track")
+    public ResponseEntity<ResponseObject> getTrack(@RequestParam long artistId) {
+        Page<Track> tracks = trackService.findByArtistId(artistId , PageRequest.of(0, 20));
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("OK", "Get "+tracks.toList().size()+" tracks of Artist successfully!", tracks.toList(), tracks.toList().size())
         );
     }
 }
--- src/main/java/com/spring/musicplayer5/dto/ResponseObject.java
+++ src/main/java/com/spring/musicplayer5/dto/ResponseObject.java
@@ -1,20 +1,25 @@
 package com.spring.musicplayer5.dto;
 
+import com.spring.musicplayer5.entity.Artist;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.lang.reflect.Array;
+import java.util.List;
 
 @Data @AllArgsConstructor
+@Builder
 @NoArgsConstructor
 public class ResponseObject {
     private String status;
     private String message;
     private Object datas;
     private int total;
+    private int page;
+    private int size;
 
-    public ResponseObject(String status , String message) {
+    public ResponseObject(String status, String message) {
         this.status = status;
         this.message = message;
     }
@@ -23,5 +28,12 @@ public class ResponseObject {
         this.status = status;
         this.message = message;
         this.datas = datas;
+    }
+
+    public ResponseObject(String status, String message, Object datas, int total) {
+        this.status = status;
+        this.message = message;
+        this.datas = datas;
+        this.total = total;
     }
 }
